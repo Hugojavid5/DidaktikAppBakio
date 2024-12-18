62aeb25f832d2a9f995227d4342aec8063547990
Hugojavid5 <hugojavidferrero@gmail.com>
2024-12-18 11:33:09 +0100

    cambios realizados

--- app/src/main/java/com/icjardinapps/dm2/bakio/Txakolina/ActividadTxakoli.kt
+++ app/src/main/java/com/icjardinapps/dm2/bakio/Txakolina/ActividadTxakoli.kt
@@ -1,7 +1,6 @@
 package com.icjardinapps.dm2.bakio.Txakolina
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.DragEvent
