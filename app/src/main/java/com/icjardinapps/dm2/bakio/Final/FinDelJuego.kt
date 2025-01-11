1eafaaba6446c6aa7f219d8a73d24d14865afb6b
Hugojavid5 <hugojavidferrero@gmail.com>
2025-01-11 09:18:13 +0100

    final del juego acabado

---
+++ app/src/main/java/com/icjardinapps/dm2/bakio/Final/FinDelJuego.kt
@@ -0,0 +1,25 @@
+package com.icjardinapps.dm2.bakio.Final
+
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.icjardinapps.dm2.bakio.R
+
+
+class FinDelJuego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_app)
+
+        val tvFinDelJuego: TextView = findViewById(R.id.tvFinDelJuego)
+        val ivAnimacion: ImageView = findViewById(R.id.ivAnimacion)
+
+        // Mostrar la imagen después de un retraso
+        Handler().postDelayed({
+            ivAnimacion.setImageResource(R.drawable.sonrisa) // Cambiar a la imagen deseada
+            ivAnimacion.visibility = ImageView.VISIBLE
+        }, 2000)
+    }
+}
