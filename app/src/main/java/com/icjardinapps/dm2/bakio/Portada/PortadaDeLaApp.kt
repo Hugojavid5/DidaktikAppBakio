94b62f402986d6482cd7ca01b69c57c4c77e1847
Guillermo <guillermo.arana1@gmail.com>
2025-01-24 14:19:31 +0100

    Portada cambiada, da igual donde clicar sa cambia a la siguiente actividad.

--- app/src/main/java/com/icjardinapps/dm2/bakio/Portada/PortadaDeLaApp.kt
+++ app/src/main/java/com/icjardinapps/dm2/bakio/Portada/PortadaDeLaApp.kt
@@ -3,7 +3,7 @@ package com.icjardinapps.dm2.bakio.Portada
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import android.view.View
 import com.icjardinapps.dm2.bakio.Bienvenida.BienvenidaApp
 import com.icjardinapps.dm2.bakio.R
 
@@ -13,11 +13,10 @@ class PortadaDeLaApp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_portada)
 
-        val buttonJugar = findViewById<Button>(R.id.buttonJugar)
-
-        // Acción para el botón "Jugar"
-        buttonJugar.setOnClickListener {
-            //Redirige a la clase Bienvenida App
+        // Configurar el evento táctil para el LinearLayout
+        val mainLayout = findViewById<View>(R.id.linearLayout)
+        mainLayout.setOnClickListener {
+            // Cambiar a otra actividad
             val intent = Intent(this, BienvenidaApp::class.java)
             startActivity(intent)
         }
