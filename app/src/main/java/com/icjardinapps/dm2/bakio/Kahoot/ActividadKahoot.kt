c32c22f7940b1bbae4809b4684bd1c33a868d874
Hugo <hugojavidferrero@gmail.com>
2025-02-04 08:32:12 +0100

    arreglado el kahoot

--- app/src/main/java/com/icjardinapps/dm2/bakio/Kahoot/ActividadKahoot.kt
+++ app/src/main/java/com/icjardinapps/dm2/bakio/Kahoot/ActividadKahoot.kt
@@ -11,35 +11,30 @@ class ActividadKahoot : AppCompatActivity() {
 
     private var currentQuestionIndex = 0
     private val userAnswers = MutableList<String?>(10) { null }
-    private val questions = listOf(
-        Question(getString(R.string.non_bizi_da_lanper), getString(R.string.udaletxean), listOf(getString(R.string.etxean), getString(R.string.udaletxean), getString(R.string.itsasoan), getString(R.string.kalean))),
-        Question(getString(R.string.non_ikusi), getString(R.string.jaietan), listOf(getString(R.string.jaietan), getString(R.string.igerilekuan), getString(R.string.herriko_plazan), getString(R.string.lagunekin_kafea_hartzen))),
-        Question(getString(R.string.zer_dago_gaztelugatxen), getString(R.string.ermita), listOf(getString(R.string.ermita), getString(R.string.altxorra), getString(R.string.taberna_bat), getString(R.string.lapurrak))),
-        Question(getString(R.string.nondik_dator_txakolina), getString(R.string.mahatsetatik), listOf(getString(R.string.sagarretik), getString(R.string.mahatsetatik), getString(R.string.platanotik), getString(R.string.ahabietatik))),
-        Question(getString(R.string.Txakolinaren_museoan), getString(R.string.gezurra), listOf(getString(R.string.egia), getString(R.string.gezurra))),
-        Question(getString(R.string.zer_da_anarrua), getString(R.string.inauterietako_pretsonaia_bat), listOf(getString(R.string.mamu_bat), getString(R.string.inauterietako_pretsonaia_bat), getString(R.string.bakioko_alkatea))),
-        Question(getString(R.string.zein_dantza_egiten_da_bakion), getString(R.string.anarruaren_zortzikoa), listOf(getString(R.string.anarruren_banakoa), getString(R.string.anarruaren_binakakoa), getString(R.string.anarruaren_launakakoa), getString(R.string.anarruaren_zortzikoa))),
-        Question(getString(R.string.san_pelaio_ermita_harriz_eraikita_dago), getString(R.string.egia), listOf(getString(R.string.egia), getString(R.string.gezurra))),
-        Question(getString(R.string.zenbat_neurtzen_du_matxitxakoko_itsasargiak), "20m", listOf("15m", "20m", "25m", "30m")),
-        Question(getString(R.string.bakioko_bela_gurutzatuak_zer_isladatzen_du), getString(R.string.piraten_itsasontzia_ondoratu_zela), listOf(getString(R.string.piratak_bakiora_iritsi_ziren_eguna), getString(R.string.piratek_bakion_lapurtu_zuten_garaia), getString(R.string.piraten_itsasontzia_ondoratu_zela), getString(R.string.ez_dauka_esanahirik)))
-    )
+    private lateinit var questions: List<Question>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kahoot)
+
+        questions = listOf(
+            Question(getString(R.string.non_bizi_da_lanper), getString(R.string.udaletxean), listOf(getString(R.string.etxean), getString(R.string.udaletxean), getString(R.string.itsasoan), getString(R.string.kalean))),
+            Question(getString(R.string.non_ikusi), getString(R.string.jaietan), listOf(getString(R.string.jaietan), getString(R.string.igerilekuan), getString(R.string.herriko_plazan), getString(R.string.lagunekin_kafea_hartzen))),
+            Question(getString(R.string.zer_dago_gaztelugatxen), getString(R.string.ermita), listOf(getString(R.string.ermita), getString(R.string.altxorra), getString(R.string.taberna_bat), getString(R.string.lapurrak))),
+            Question(getString(R.string.nondik_dator_txakolina), getString(R.string.mahatsetatik), listOf(getString(R.string.sagarretik), getString(R.string.mahatsetatik), getString(R.string.platanotik), getString(R.string.ahabietatik))),
+            Question(getString(R.string.Txakolinaren_museoan), getString(R.string.gezurra), listOf(getString(R.string.egia), getString(R.string.gezurra))),
+            Question(getString(R.string.zer_da_anarrua), getString(R.string.inauterietako_pretsonaia_bat), listOf(getString(R.string.mamu_bat), getString(R.string.inauterietako_pretsonaia_bat), getString(R.string.bakioko_alkatea))),
+            Question(getString(R.string.zein_dantza_egiten_da_bakion), getString(R.string.anarruaren_zortzikoa), listOf(getString(R.string.anarruren_banakoa), getString(R.string.anarruaren_binakakoa), getString(R.string.anarruaren_launakakoa), getString(R.string.anarruaren_zortzikoa))),
+            Question(getString(R.string.san_pelaio_ermita_harriz_eraikita_dago), getString(R.string.egia), listOf(getString(R.string.egia), getString(R.string.gezurra))),
+            Question(getString(R.string.zenbat_neurtzen_du_matxitxakoko_itsasargiak), "20m", listOf("15m", "20m", "25m", "30m")),
+            Question(getString(R.string.bakioko_bela_gurutzatuak_zer_isladatzen_du), getString(R.string.piraten_itsasontzia_ondoratu_zela), listOf(getString(R.string.piratak_bakiora_iritsi_ziren_eguna), getString(R.string.piratek_bakion_lapurtu_zuten_garaia), getString(R.string.piraten_itsasontzia_ondoratu_zela), getString(R.string.ez_dauka_esanahirik)))
+        )
 
         val questionTextView = findViewById<TextView>(R.id.textViewQuestion)
         val radioGroupAnswers = findViewById<RadioGroup>(R.id.radioGroupAnswers)
         val nextQuestionButton = findViewById<Button>(R.id.buttonNextQuestion)
         val previousQuestionButton = findViewById<Button>(R.id.buttonPreviousQuestion)
         val backToWelcomeButton = findViewById<Button>(R.id.buttonBackToWelcome)
-
-        // Log para depurar la inicialización de las vistas
-        Log.d("ActividadKahoot", "questionTextView: $questionTextView")
-        Log.d("ActividadKahoot", "radioGroupAnswers: $radioGroupAnswers")
-        Log.d("ActividadKahoot", "nextQuestionButton: $nextQuestionButton")
-        Log.d("ActividadKahoot", "previousQuestionButton: $previousQuestionButton")
-        Log.d("ActividadKahoot", "backToWelcomeButton: $backToWelcomeButton")
 
         showQuestion(questionTextView, radioGroupAnswers)
         updateButtonVisibility()
@@ -110,29 +105,10 @@ class ActividadKahoot : AppCompatActivity() {
         val checkAnswersButton = findViewById<Button>(R.id.buttonCheckAnswers)
         val backToWelcomeButton = findViewById<Button>(R.id.buttonBackToWelcome)
 
-        previousQuestionButton.isEnabled = false
-        checkAnswersButton.isEnabled = false
-        nextQuestionButton.isEnabled = true
-        backToWelcomeButton.isEnabled = true
-
-        when (currentQuestionIndex) {
-            0 -> {
-                previousQuestionButton.isEnabled = false
-                backToWelcomeButton.isEnabled = true
-            }
-            questions.size - 1 -> {
-                nextQuestionButton.isEnabled = false
-                checkAnswersButton.isEnabled = true
-                previousQuestionButton.isEnabled = true
-                backToWelcomeButton.isEnabled = false
-            }
-            else -> {
-                previousQuestionButton.isEnabled = true
-                nextQuestionButton.isEnabled = true
-                checkAnswersButton.isEnabled = false
-                backToWelcomeButton.isEnabled = false
-            }
-        }
+        previousQuestionButton.isEnabled = currentQuestionIndex > 0
+        nextQuestionButton.isEnabled = currentQuestionIndex < questions.size - 1
+        checkAnswersButton.isEnabled = currentQuestionIndex == questions.size - 1
+        backToWelcomeButton.isEnabled = currentQuestionIndex == 0
 
         checkAnswersButton.setOnClickListener {
             showResults()
@@ -148,7 +124,7 @@ class ActividadKahoot : AppCompatActivity() {
 
         userAnswers.forEachIndexed { index, answer ->
             val question = questions[index]
-            val displayedAnswer = answer ?: "${question.correctAnswer}"
+            val displayedAnswer = answer ?: question.correctAnswer
             val answerTextView = TextView(this).apply {
                 text = "Pregunta ${index + 1}: $displayedAnswer"
                 textSize = 18f
